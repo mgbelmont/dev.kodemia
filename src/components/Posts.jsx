715a954f714e44a710c980e11bfdf066e2cf56cb
Mary <oxesescom@gmail.com>
2021-06-09 19:40:05 -0500

    bus de datos en posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,32 +1,42 @@
 import React from "react";
 import AppCard from "./AppCard";
 
+const postObject = {
+  post1: [
+    {
+      title: "Card Title 1",
+      text: "lorem loremo lorem loremo lorem loremo lorem loremo",
+    },
+  ],
+  post2: [
+    {
+      title: "Card Title 2",
+      text: "lorem loremo lorem loremo lorem loremo lorem loremo",
+    },
+  ],
+  post3: [
+    {
+      title: "Card Title 3",
+      text: "lorem loremo lorem loremo lorem loremo lorem loremo",
+    },
+  ],
+};
+
 export default function Posts(props) {
-  return (
-    <div>
-      <AppCard>
-        <h5 class="card-title">Card Title</h5>
-        <p class="card-text">Content card</p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
-      </AppCard>
-      <AppCard>
-        <h5 class="card-title">Card Title</h5>
-        <p class="card-text">Content card</p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
-      </AppCard>
-      <AppCard>
-        <h5 class="card-title">Card Title</h5>
-        <p class="card-text">Content card</p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
-      </AppCard>
-    </div>
-  );
+  const buildLi = ({ title, text }, index) => {
+    return (
+      <li>
+        <AppCard>
+          <h5 class="card-title">title</h5>
+          <p class="card-text">text</p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </AppCard>
+      </li>
+    );
+  };
+  return <ul>{Object.entries(postObject).map(buildLi)}</ul>;
 }
 /*
 class Posts extends React.Component{
