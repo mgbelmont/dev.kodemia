2ebb3df55328242af4515ac126d74d8ff9791247
Mary <oxesescom@gmail.com>
2021-06-09 19:17:44 -0500

    refactor posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
-import AppCard from './AppCard'
+import React from "react";
+import AppCard from "./AppCard";
 
+export default function Posts(props) {
+  return (
+    <div>
+      <AppCard>
+        <h5 class="card-title">Card Title</h5>
+        <p class="card-text">Content card</p>
+        <a href="#" class="btn btn-primary">
+          Go somewhere
+        </a>
+      </AppCard>
+      <AppCard>
+        <h5 class="card-title">Card Title</h5>
+        <p class="card-text">Content card</p>
+        <a href="#" class="btn btn-primary">
+          Go somewhere
+        </a>
+      </AppCard>
+      <AppCard>
+        <h5 class="card-title">Card Title</h5>
+        <p class="card-text">Content card</p>
+        <a href="#" class="btn btn-primary">
+          Go somewhere
+        </a>
+      </AppCard>
+    </div>
+  );
+}
+/*
 class Posts extends React.Component{
     constructor(props){
         super(props)
@@ -32,6 +60,4 @@ class Posts extends React.Component{
         
         )
     }
-}
-
-export default Posts
+}*/
