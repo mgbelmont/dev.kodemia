11edf70e568c44af6f701057e2c4720d70095db2
Mary <oxesescom@gmail.com>
2021-06-09 21:12:36 -0500

    rick and morty API

--- src/screens/Exercises/index.jsx
+++ src/screens/Exercises/index.jsx
@@ -1,5 +1,129 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
+const rick = [
+  {
+    id: 1,
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+    origin: {
+      name: "Earth (C-137)",
+      url: "https://rickandmortyapi.com/api/location/1",
+    },
+    location: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    episode: [
+      "https://rickandmortyapi.com/api/episode/1",
+      "https://rickandmortyapi.com/api/episode/2",
+      "https://rickandmortyapi.com/api/episode/3",
+      "https://rickandmortyapi.com/api/episode/4",
+      "https://rickandmortyapi.com/api/episode/5",
+      "https://rickandmortyapi.com/api/episode/6",
+      "https://rickandmortyapi.com/api/episode/7",
+      "https://rickandmortyapi.com/api/episode/8",
+      "https://rickandmortyapi.com/api/episode/9",
+      "https://rickandmortyapi.com/api/episode/10",
+      "https://rickandmortyapi.com/api/episode/11",
+      "https://rickandmortyapi.com/api/episode/12",
+      "https://rickandmortyapi.com/api/episode/13",
+      "https://rickandmortyapi.com/api/episode/14",
+      "https://rickandmortyapi.com/api/episode/15",
+      "https://rickandmortyapi.com/api/episode/16",
+      "https://rickandmortyapi.com/api/episode/17",
+      "https://rickandmortyapi.com/api/episode/18",
+      "https://rickandmortyapi.com/api/episode/19",
+      "https://rickandmortyapi.com/api/episode/20",
+      "https://rickandmortyapi.com/api/episode/21",
+      "https://rickandmortyapi.com/api/episode/22",
+      "https://rickandmortyapi.com/api/episode/23",
+      "https://rickandmortyapi.com/api/episode/24",
+      "https://rickandmortyapi.com/api/episode/25",
+      "https://rickandmortyapi.com/api/episode/26",
+      "https://rickandmortyapi.com/api/episode/27",
+      "https://rickandmortyapi.com/api/episode/28",
+      "https://rickandmortyapi.com/api/episode/29",
+      "https://rickandmortyapi.com/api/episode/30",
+      "https://rickandmortyapi.com/api/episode/31",
+      "https://rickandmortyapi.com/api/episode/32",
+      "https://rickandmortyapi.com/api/episode/33",
+      "https://rickandmortyapi.com/api/episode/34",
+      "https://rickandmortyapi.com/api/episode/35",
+      "https://rickandmortyapi.com/api/episode/36",
+      "https://rickandmortyapi.com/api/episode/37",
+      "https://rickandmortyapi.com/api/episode/38",
+      "https://rickandmortyapi.com/api/episode/39",
+      "https://rickandmortyapi.com/api/episode/40",
+      "https://rickandmortyapi.com/api/episode/41",
+    ],
+    url: "https://rickandmortyapi.com/api/character/1",
+    created: "2017-11-04T18:48:46.250Z",
+  },
+];
+
+export default function Exercises() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  const build = (
+    [
+      postion,
+      {
+        name,
+        status,
+        species,
+        type,
+        gender,
+        origin,
+        location,
+        image,
+        episode,
+        url,
+        created,
+      },
+    ],
+    index
+  ) => {
+    //console.log(name);
+    return (
+      <React.Fragment key={index}>
+        <div className="card" style={{ width: "18rem" }}>
+          <img class="card-img-top" src={image} alt="Card image cap" />
+          <div class="card-body">
+            <h5 class="card-title">{name}</h5>
+            <p class="card-text"></p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">{status}</li>
+            <li class="list-group-item">{species}</li>
+            <li class="list-group-item">{gender}</li>
+            <li class="list-group-item">{type}</li>
+          </ul>
+        </div>
+      </React.Fragment>
+    );
+  };
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col">
+          {data.results ? Object.entries(data.results).map(build) : build([])}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*
 const petsData = {
   dogs: [
     {
@@ -48,7 +172,7 @@ export default function Exercises() {
       </div>
     </div>
   );
-}
+}*/
 
 /*
 export default function Exercises() {
