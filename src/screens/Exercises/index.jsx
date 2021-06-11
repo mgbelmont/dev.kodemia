48e2a95f59cd34fb8629ae8c390a09ea08b751ff
Mary <oxesescom@gmail.com>
2021-06-10 19:16:57 -0500

    paginacion

--- src/screens/Exercises/index.jsx
+++ src/screens/Exercises/index.jsx
@@ -1,56 +1,162 @@
 import React, { useState, useEffect } from "react";
 
+const rick = [
+  {
+    id: 1,
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+    origin: {
+      name: "Earth (C-137)",
+      url: "https://rickandmortyapi.com/api/location/1",
+    },
+    location: {
+      name: "Earth (Replacement Dimension)",
+      url: "https://rickandmortyapi.com/api/location/20",
+    },
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    episode: [
+      "https://rickandmortyapi.com/api/episode/1",
+      "https://rickandmortyapi.com/api/episode/2",
+      "https://rickandmortyapi.com/api/episode/3",
+      "https://rickandmortyapi.com/api/episode/4",
+      "https://rickandmortyapi.com/api/episode/5",
+      "https://rickandmortyapi.com/api/episode/6",
+      "https://rickandmortyapi.com/api/episode/7",
+      "https://rickandmortyapi.com/api/episode/8",
+      "https://rickandmortyapi.com/api/episode/9",
+      "https://rickandmortyapi.com/api/episode/10",
+      "https://rickandmortyapi.com/api/episode/11",
+      "https://rickandmortyapi.com/api/episode/12",
+      "https://rickandmortyapi.com/api/episode/13",
+      "https://rickandmortyapi.com/api/episode/14",
+      "https://rickandmortyapi.com/api/episode/15",
+      "https://rickandmortyapi.com/api/episode/16",
+      "https://rickandmortyapi.com/api/episode/17",
+      "https://rickandmortyapi.com/api/episode/18",
+      "https://rickandmortyapi.com/api/episode/19",
+      "https://rickandmortyapi.com/api/episode/20",
+      "https://rickandmortyapi.com/api/episode/21",
+      "https://rickandmortyapi.com/api/episode/22",
+      "https://rickandmortyapi.com/api/episode/23",
+      "https://rickandmortyapi.com/api/episode/24",
+      "https://rickandmortyapi.com/api/episode/25",
+      "https://rickandmortyapi.com/api/episode/26",
+      "https://rickandmortyapi.com/api/episode/27",
+      "https://rickandmortyapi.com/api/episode/28",
+      "https://rickandmortyapi.com/api/episode/29",
+      "https://rickandmortyapi.com/api/episode/30",
+      "https://rickandmortyapi.com/api/episode/31",
+      "https://rickandmortyapi.com/api/episode/32",
+      "https://rickandmortyapi.com/api/episode/33",
+      "https://rickandmortyapi.com/api/episode/34",
+      "https://rickandmortyapi.com/api/episode/35",
+      "https://rickandmortyapi.com/api/episode/36",
+      "https://rickandmortyapi.com/api/episode/37",
+      "https://rickandmortyapi.com/api/episode/38",
+      "https://rickandmortyapi.com/api/episode/39",
+      "https://rickandmortyapi.com/api/episode/40",
+      "https://rickandmortyapi.com/api/episode/41",
+    ],
+    url: "https://rickandmortyapi.com/api/character/1",
+    created: "2017-11-04T18:48:46.250Z",
+  },
+];
+
 export default function Exercises() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
+  const [prev, setPrev] = useState(null);
+  const [next, setNext] = useState(null);
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
       .then((response) => response.json())
-      .then((json) => setData(json));
+      .then((json) => {
+        setData(json.results);
+        setPrev(json.info.prev);
+        setNext(json.info.next);
+      });
   }, []);
 
   const build = (
-    [
-      postion,
-      {
-        name,
-        status,
-        species,
-        type,
-        gender,
-        origin,
-        location,
-        image,
-        episode,
-        url,
-        created,
-      },
-    ],
+    {
+      name,
+      status,
+      species,
+      type,
+      gender,
+      origin,
+      location,
+      image,
+      episode,
+      url,
+      created,
+    },
+
     index
   ) => {
     return (
       <React.Fragment key={index}>
         <div className="card" style={{ width: "18rem" }}>
-          <img class="card-img-top" src={image} alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">{name}</h5>
-            <p class="card-text"></p>
+          <img className="card-img-top" src={image} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text"></p>
           </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">{status}</li>
-            <li class="list-group-item">{species}</li>
-            <li class="list-group-item">{gender}</li>
-            <li class="list-group-item">{type}</li>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">{status}</li>
+            <li className="list-group-item">{species}</li>
+            <li className="list-group-item">{gender}</li>
           </ul>
         </div>
       </React.Fragment>
     );
   };
+  const getNextPage = () => {
+    fetch(next)
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json.results);
+        setPrev(json.info.prev);
+        setNext(json.info.next);
+      });
+  };
+
+  const getPrevPage = () => {
+    fetch(prev)
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json.results);
+        setPrev(json.info.prev);
+        setNext(json.info.next);
+      });
+  };
+
   return (
     <div className="container">
-      <div className="row justify-content-center">
-        <div className="col">
-          {data.results ? Object.entries(data.results).map(build) : build([])}
+      <div className="row justify-content-center d-flex p-2">
+        <div className="col">{data.map(build)}</div>
+      </div>
+      <div className="row">
+        <div className="col justify-content-center d-flex flex-wrap p-5">
+          <button
+            disabled={prev === null ? true : false}
+            type="button"
+            className="btn btn-dark btn-lg mx-3"
+            onClick={getPrevPage}
+          >
+            Prev
+          </button>
+          <button
+            disabled={next === null ? true : false}
+            type="button"
+            className="btn btn-dark btn-lg mx-3"
+            onClick={getNextPage}
+          >
+            Next
+          </button>
         </div>
       </div>
     </div>
